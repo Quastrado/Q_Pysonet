1ca705c9481cdd2744df41474bfd7d3bdb56eabf
Quastrado <quastrado@gmail.com>
2021-05-06 17:53:17 +0300

    config changes

--- Q_pysonet/config/settings.py
+++ Q_pysonet/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
+    'src.profiles'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    
+    ),
+}
+
+
+SIMPLE_JWT = {
+#    'AUTH_HEADER_TYPES': ('JWT',),
+}
